558953d3c3afc4c09c82793baff6f873a99467d5
IhebBenTaher <iheb.ben.taher1@gmail.com>
2025-05-26 22:31:53 +0100

    update registry v2

--- registry/src/main/java/com/example/registry/RegistryApplication.java
+++ registry/src/main/java/com/example/registry/RegistryApplication.java
@@ -3,11 +3,17 @@ package com.example.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class RegistryApplication {
-
+	@GetMapping
+	public String hello(){
+		return "hello from registry";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryApplication.class, args);
 	}
